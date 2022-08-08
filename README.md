c08dec092688ba692a7a254e93d1ea5493c3ef35
Sergey_Osipov <sibkron@mail.ru>
2022-08-08 10:53:41 +0700

    telefram cost bot

---
+++ README.md
@@ -0,0 +1,1 @@
+# Telegram Cost Bot
---
+++ const.js
@@ -0,0 +1,19 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`;
+
+const text1 = `
+1 Текст для проверки обработчика <a href="https://youtube.com/">Ссылка</a>
+`;
+const text2 = `
+2 Текст для проверки обработчика <a href="https://youtube.com/">Ссылка</a>
+`;
+const text3 = `
+3 Текст для проверки обработчика <a href="https://youtube.com/">Ссылка</a>
+`;
+
+module.exports.commands = commands;
+module.exports.text1 = text1;
+module.exports.text2 = text2;
+module.exports.text3 = text3;
